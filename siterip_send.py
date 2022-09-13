13909fd84c71552cf07ff4dd411d581ee32bd05c
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2022-09-13 21:00:47 +0530

    Add files via upload

---
+++ siterip_send.py
@@ -0,0 +1,25 @@
+# code to send links from final_links to Telegram chat
+from pyrogram import Client
+from time import sleep
+
+app = Client("my_account2")
+
+links = []
+print("Restarting")
+f = open("final_links.txt", "r")
+for i in f:
+    links.append("/mirror " + i[:len(i) - 1])
+f.close()
+while True:
+    for i in links:
+        async def main():
+            async with app:
+                print(i)
+                await app.send_message(-1001548796191, i)
+
+
+        app.run(main())
+
+        sleep(1)
+
+#bhfpodwfdr
