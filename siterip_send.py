00785dcc05c00df6d80e3d212e5c9e4f03dcfa31
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2022-09-16 23:17:45 +0530

    Update siterip_send.py

--- siterip_send.py
+++ siterip_send.py
@@ -6,7 +6,7 @@ app = Client("my_account2")
 
 links = []
 print("Restarting")
-f = open("/links/final_links_000.txt", "r")
+f = open("links/final_links_000.txt", "r")
 for i in f:
     links.append("/mirror " + i[:len(i) - 1])
 f.close()
