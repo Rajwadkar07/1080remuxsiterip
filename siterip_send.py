2dc1e915550028146adfd6a4431d8a95f09a13e6
Rajwadkar07 <53391570+Rajwadkar07@users.noreply.github.com>
2022-09-17 07:08:32 +0530

    Update siterip_send.py

--- siterip_send.py
+++ siterip_send.py
@@ -1,12 +1,13 @@
 # code to send links from final_links to Telegram chat
 from pyrogram import Client
 from time import sleep
+import os
 
 app = Client("my_account2")
 
 links = []
 print("Restarting")
-f = open("links/final_links_000.txt", "r")
+f = open(os.getcwd() + "/final_links_000.txt", "r")
 for i in f:
     links.append("/mirror " + i[:len(i) - 1])
 f.close()
